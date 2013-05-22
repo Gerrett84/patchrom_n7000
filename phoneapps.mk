f8384f44d5281b859b74a7582a6baecb88e2faf2
Gerrett84 <Gerrett84@gmail.com>
2013-05-22 21:49:12 +0200

    Initial release ..

---
+++ phoneapps.mk
@@ -0,0 +1,108 @@
+local-phone-apps = $(private-phone-apps) $(private-services)
+private-services := AllshareService \
+					FlashBarService \
+					SuggestionService \
+					QuickSnote \
+					SnoteT0Provider \
+					ResourceManager \
+					SPenGestuurePad \
+					IdeaVisualizer \
+					QuickSnote \
+					SamsungIME \
+
+private-phone-apps := ApplicationsProvider \
+					  BackupRestoreConfirmation \
+					  BadgeProvider \
+					  BluetoothAvrcp \
+					  BluetoothMap \
+					  BluetoothTest \
+					  CSC \
+					  ClipboardSaveService \
+					  CertInstaller \
+					  DefaultContainerService \
+					  Divx \
+					  EdmVpnServices \
+					  FaceLock \
+					  FactoryTest \
+					  FmRadio \
+					  Gallery2 \
+					  KeyChain \
+					  LatinIME \
+					  LogsProvider \
+					  MediaUploader \
+					  MusicFX \
+					  MtpApplication \
+					  OmaCP \
+					  PCWClientS \
+					  PRUI \
+					  PanningTryActually \
+					  Personalization \
+					  PhoneErrService \
+					  PopupBrowser \
+					  PolarisViewer \
+					  PopupuiReceiver \
+					  Preconfig \
+					  PreloadInstaller \
+					  SamsungIME \
+					  SamsungCamera \
+					  SamsungTTS \
+					  SecMediaProvider \
+					  SecHTMLViewer \
+					  SecLiveWallpapersPicker \
+					  SecurityProvider \
+					  SelfTestMode \
+					  Stk \
+					  UserDictionaryProvider \
+					  VpnDialogs \
+					  VideoPlayer \
+					  WAPPushManager \
+					  WlanTest \
+					  minimode-res \
+					  serviceModeApp \
+					  wssyncmlnps \
+					  Cusettings
+
+private-lockscreen-apps := PickUpTutorial \
+						   AccuweatherDaemon \
+						   AccuweatherWidget \
+						   AccuweathetWidget_Main
+
+private-phone-apps2 :=  AllshareDMC \
+						AllshareMediaServer \
+						AllSharePlay \
+						BGCleaner \
+						BroadcomMEMSService \
+						CapabilityManagerService \
+						CertInstaller \
+						ChromeBookmarksSyncAdapter \
+						ContextAwareService \
+						CWMManager \
+						DeepSea \
+						DirectShareManager \
+						DrmProvider \
+						DSMForwarding \
+						DSMLawmo \
+						EdmSysScopeService \
+						EdmVpnServices \
+						EnterprisePermissions \
+						Hearingdro \
+						INDI_Server \
+						JobManager \
+						NetworkLocation \
+						NoiseField \
+						OmaCP \
+						OmaDrmPopup \
+						PackageInstaller \
+						PanningTryActually \
+						PCWClientS \
+						PhaseBeam \
+						PRUI \
+						SecurityProvider \
+						ShaoNv \
+						TwDVFSApp \
+						VpnDialogs \
+						WfdBroker \
+						BluetoothAvrcp \
+						BluetoothMap \
+						BluetoothTest
+#DreyeforSamsung
